1f8ed3d01204f13b87ccac2b13736a471c06f190
藤田優作 <marozaemon1217@gmail.com>
2021-01-12 12:51:00 +0900

    コミット

---
+++ Apptest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apptest2
+//
+//  Created by 藤田優作 on 2021/01/08.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
